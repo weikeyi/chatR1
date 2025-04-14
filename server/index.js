0ead43a23d8f46d8f0e5107ca687285ebcee51a5
中华小当家 <2293388768@qq.com>
2025-04-14 19:33:33 +0800

    first

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
+require('dotenv').config(); // 在代码顶部加载 .env 文件中的环境变量
 const fs = require('fs')
 const express = require('express')
 const cors = require('cors')
+const OpenAI = require('openai')
 
 const app = express()
 app.use(cors())
@@ -10,6 +12,8 @@ const filePath = './demo.txt'
 let lastSize = fs.statSync(filePath).size;
 
 port = process.env.PORT || 3000
+API = process.env.API
+
 
 app.get('/sse',(req,res)=>{
     res.writeHead(200,{
@@ -82,7 +86,43 @@ fs.watch(filePath, { persistent: true }, (eventType, filename) => {
         console.error('File stat error:', err);
       }
     }
-  });
+  })
+
+async function requestOpenAI(message) {
+  let response
+  try{
+    const completion = await OpenAI.chat.completions.create({
+      model: "deepseek-r1",  //模型列表：https://help.aliyun.com/zh/model-studio/getting-started/models
+      messages: [
+          { role: "system", content: "You are a helpful assistant." },
+          { role: "user", content: message }
+      ],
+      stream: true
+  })
+    response = completion
+    for await (const chunk of completion) {
+      console.log(chunk);
+      
+    }
+    
+  }catch(error){
+    console.error("处理 /chat 请求时出错:", error)
+  }
+  return response
+}
+
+requestOpenAI('你好')
+
+app.post('/chat',async (req,res)=>{
+  res.setHeader('Content-Type','text/event-stream')
+  res.setHeader('Cache-Control','no-cache')
+  res.setHeader('Connection','keep-alive')
+
+  
+    const message = req.body.message
+    const response = await requestOpenAI(message)
+    res.json(response)
+})
 
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`); 
